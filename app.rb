cb7126c662a7511e93bc6e2c39d285b956f6c693
Alex Khrustalev <alexkhrust@gmail.com>
2013-11-19 15:13:21 +0200

    root action

--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 
-get '/' do
-  'Hello world!'
-end
-
 post '/whatweb' do
   if !params[:target].nil? && !params[:target].empty?
     whatweb_binary = File.join(File.expand_path('..', __FILE__), 'whatweb/whatweb')
