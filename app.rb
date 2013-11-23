1cc628aac8623ba8940326f8232cfc10c246bdbe
Ivan Linko <linko.ivan@gmail.com>
2013-11-23 02:53:51 +0200

    Fix whatweb path

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 class WhatWebApp < Sinatra::Base
   get '/whatweb' do
     if !params[:target].nil? && !params[:target].empty?
-      whatweb_binary = File.join(File.expand_path('..', __FILE__), 'whatweb/whatweb')
+      whatweb_binary = File.join(File.expand_path('..', __FILE__), 'WhatWeb/whatweb')
       output = `#{whatweb_binary} #{params[:target]} --color=never`
       output.empty? ? "Unknown" : output
     end
